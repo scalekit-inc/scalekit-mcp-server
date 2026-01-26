d8dc1e038a900d7da97a2dea513e19a38131b47d
Akshay Parihar <akshay.parihar@scalekit.com>
2026-01-26 20:21:04 +0530

    updates

--- src/scalekit.ts
+++ src/scalekit.ts
@@ -2,7 +2,6 @@ import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { Scalekit, TokenValidationOptions } from '@scalekit-sdk/node';
 import cors from 'cors';
 import express from 'express';
-import https from "https";
 import { config } from './config/config.js';
 import { oauthProtectedResourceHandler } from './lib/auth.js';
 import { logger } from './lib/logger.js';
@@ -27,14 +26,14 @@ const allowAll = cors({
 app.options(/.*/, allowAll);
 app.use(allowAll);
 
-app.get("/favicon.ico", (_req, res) => {
-  https.get("https://cdn.scalekit.com/assets/mcp/scalekit-fav.png", (cdnRes) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", cdnRes.headers["content-type"] || "image/png");
-    res.setHeader("Cache-Control", "public, max-age=86400");
-    cdnRes.pipe(res);
-  }).on("error", () => res.sendStatus(502));
-});
+// app.get("/favicon.ico", (_req, res) => {
+//   https.get("https://cdn.scalekit.com/assets/mcp/scalekit-fav.png", (cdnRes) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", cdnRes.headers["content-type"] || "image/png");
+//     res.setHeader("Cache-Control", "public, max-age=86400");
+//     cdnRes.pipe(res);
+//   }).on("error", () => res.sendStatus(502));
+// });
 
 
 // Serve static files (including the HTML with favicon)
@@ -51,6 +50,24 @@ const scalekit = new Scalekit(config.skEnvUrl, config.skClientId, config.skClien
 
   app.use(async (req, res, next) => {
     try {
+
+      const ua = String(req.headers['user-agent'] || '').toLowerCase();
+      const isGoogleFaviconBot = ua.includes('gstatic');
+      if (isGoogleFaviconBot) {
+      return res
+        .status(200)
+        .type('text/html')
+        .send(`<!doctype html>
+              <html>
+              <head>
+                <meta charset="utf-8" />
+                <link rel="icon" href="https://cdn.scalekit.com/assets/mcp/scalekit-fav.png" />
+                <title>Scalekit MCP</title>
+              </head>
+              <body>Scalekit MCP</body>
+              </html>`);
+      }
+      
       // Allow public access to well-known endpoints
       if (req.path.includes('.well-known') || req.path === '/favicon.ico') {
         return next();
